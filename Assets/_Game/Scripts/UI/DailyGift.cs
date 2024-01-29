1d55831f130e8f67ccb5a42b9d45bcc6346b9e6f
Yamassi <karateka_marateka@mail.ru>
2024-01-29 13:11:30 +0600

    AddDailyGiftsUI

---
+++ Assets/_Game/Scripts/UI/DailyGift.cs
@@ -0,0 +1,32 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DailyGift : MonoBehaviour
+{
+    [SerializeField] private int _id;
+    [SerializeField] private int _reward;
+    [SerializeField] private Button _open;
+    [SerializeField] private TextMeshProUGUI _rewardText;
+    public Action<int,int> OnSelect;
+    private void OnValidate()
+    {
+        _open = GetComponentInChildren<Button>(true);
+        _rewardText = GetComponentInChildren<TextMeshProUGUI>();
+        _rewardText.text = _reward.ToString();
+        _id = transform.GetSiblingIndex();
+    }
+
+    private void OnEnable()
+    {
+        _open.onClick.AddListener(Clicked);
+    }
+
+    private void OnDisable()
+    {
+        _open.onClick.RemoveListener(Clicked);
+    }
+
+    private void Clicked() => OnSelect?.Invoke(_id,_reward);
+}
